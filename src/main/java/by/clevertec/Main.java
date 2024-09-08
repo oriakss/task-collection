bb2966f3279cb97234713bcf3430a8a0ff7789d4
oriakss <hellishsaiyan@outlook.com>
2024-09-09 02:14:58 +0300

    task10 completed

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -123,7 +123,10 @@ public class Main {
 
     public static void task10() {
         List<Animal> animals = Util.getAnimals();
-//        animals.stream() Продолжить ...
+        animals.stream()
+                .map(Animal::getAge)
+                .reduce(Integer::sum)
+                .ifPresent(System.out::println);
     }
 
     public static void task11() {
